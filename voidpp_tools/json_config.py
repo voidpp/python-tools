ef62cec8673f255dd9ce909d23a877ba93bd6bf5
Lajos Santa <santa.lajos@coldline.hu>
2015-11-18 21:41:35 +0100

    Raise exception in case of config load error

--- voidpp_tools/json_config.py
+++ voidpp_tools/json_config.py
@@ -3,20 +3,22 @@ import os
 import json
 
 class JSONConfigLoader():
-    def __init__(self):
+    def __init__(self, base_path):
         self.sources = [
             os.path.dirname(os.getcwd()),
-            os.path.dirname(os.path.abspath(__file__)),
+            os.path.dirname(os.path.abspath(base_path)),
             os.path.expanduser('~'),
             '/etc',
         ]
 
     def load(self, filename):
+        tries = []
         for source in self.sources:
             file_path = os.path.join(source, filename)
+            tries.append(file_path)
             if not os.path.exists(file_path):
                 continue
             with open(file_path) as f:
                 return json.load(f)
 
-        return None
+        raise Exception("Config file not found in: %s" % tries)
