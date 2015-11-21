61ee786137fb552e1e5e5d86f8c05cea9219a503
Lajos Santa <santa.lajos@coldline.hu>
2015-11-21 11:23:40 +0100

    Add save to config loader

--- setup.py
+++ setup.py
@@ -3,5 +3,14 @@ from setuptools import setup, find_packages
 setup(
     name = "voidpp-tools",
     desciption = "various python tools",
-    version = "1.0.0",
+    version = "1.1.0",
+    author = 'Lajos Santa',
+    author_email = 'santa.lajos@coldline.hu',
+    url = 'https://github.com/voidpp/python-tools.git',
+    install_requires = [
+    ],
+    scripts = [
+    ],
+    include_package_data = True,
+    packages = find_packages(),
 )
--- voidpp_tools/json_config.py
+++ voidpp_tools/json_config.py
@@ -2,6 +2,9 @@
 import os
 import json
 
+class ConfigLoaderException(Exception):
+    pass
+
 class JSONConfigLoader():
     def __init__(self, base_path):
         self.sources = [
@@ -10,15 +13,29 @@ class JSONConfigLoader():
             os.path.expanduser('~'),
             '/etc',
         ]
+        self.__loaded_config_file = None
 
-    def load(self, filename):
+    def load(self, filename, create = None, default_conf = {}):
         tries = []
         for source in self.sources:
             file_path = os.path.join(source, filename)
             tries.append(file_path)
             if not os.path.exists(file_path):
                 continue
+            self.__loaded_config_file = file_path
             with open(file_path) as f:
                 return json.load(f)
 
-        raise Exception("Config file not found in: %s" % tries)
+        if create is not None:
+            self.__loaded_config_file = os.path.join(create, filename)
+            self.save(default_conf)
+            return default_conf
+
+        raise ConfigLoaderException("Config file not found in: %s" % tries)
+
+    def save(self, config):
+        if self.__loaded_config_file is None:
+            raise ConfigLoaderException("Load not called yet!")
+
+        with open(self.__loaded_config_file, 'w') as f:
+            f.write(json.dumps(config))
