2aad656c61241639c03d538498f04e5dae1b834d
Lajos Santa <santa.lajos@coldline.hu>
2016-04-07 20:49:49 +0200

    Add nested option for ConfigLoader

--- voidpp_tools/config_loader.py
+++ voidpp_tools/config_loader.py
@@ -1,5 +1,6 @@
 import os
 import abc
+from .dict_utils import recursive_update
 
 class ConfigLoaderException(Exception):
     pass
@@ -35,8 +36,9 @@ class ConfigLoader(object):
         Args:
             formatter (ConfigFormatter): config encode/decode interface instance
             base_path (str): a custom path which may be contains the config
+            nested (bool): in case of true, load all the config files found in the sources and merges it
     """
-    def __init__(self, formatter, base_path):
+    def __init__(self, formatter, base_path, nested = False):
         self.sources = [
             os.path.dirname(os.getcwd()),
             os.path.dirname(os.path.abspath(base_path)),
@@ -45,18 +47,41 @@ class ConfigLoader(object):
         ]
         self.__loaded_config_file = None
         self.__formatter = formatter
+        self.__nested = nested
 
     @property
     def filename(self):
         return self.__loaded_config_file
+
+    def __search_config_files(self, filename):
+        filenames = []
+        tries = []
+        for source in self.sources:
+            file_path = os.path.join(source, filename)
+            tries.append(file_path)
+            if not os.path.exists(file_path):
+                continue
+            filenames.append(file_path)
+
+        return filenames, tries
+
+    def __load_config_files(self, filenames):
+        data = dict()
+
+        for filename in filenames:
+            with open(filename) as f:
+                recursive_update(data, self.__formatter.decode(f.read()))
+
+        return data
 
     def load(self, filename, create = None, default_conf = {}):
         """Load the config file
 
         Args:
             filename (str): the filename of the config, without any path
-            create (Optional[str]): if the config file not found, and this parameter is not None, a config file will be create with content of default_conf
-            default_conf (Optional[dict]): content of the default config data
+            create (str): if the config file not found, and this parameter is not None,
+                          a config file will be create with content of default_conf
+            default_conf (dict): content of the default config data
 
         Returns:
             Return value of the ConfigFormatter.decode or the default_conf value
@@ -65,15 +90,11 @@ class ConfigLoader(object):
             ConfigLoaderException: if the config file not found
 
         """
-        tries = []
-        for source in self.sources:
-            file_path = os.path.join(source, filename)
-            tries.append(file_path)
-            if not os.path.exists(file_path):
-                continue
-            self.__loaded_config_file = file_path
-            with open(file_path) as f:
-                return self.__formatter.decode(f.read())
+        filenames, tries = self.__search_config_files(filename)
+
+        if len(filenames):
+            self.__loaded_config_file = filenames if self.__nested else filenames[0]
+            return self.__load_config_files(filenames if self.__nested else filenames[:1])
 
         if create is not None:
             self.__loaded_config_file = os.path.join(create, filename)
@@ -89,9 +110,12 @@ class ConfigLoader(object):
             data: any serializable config data
 
         Raises:
-            ConfigLoaderException: if the ConfigLoader.load not called, so there is no config file name, or the data is not serializable
-
+            ConfigLoaderException: if the ConfigLoader.load not called, so there is no config file name,
+                                   or the data is not serializable or the loader is nested
         """
+        if self.__nested:
+            raise ConfigLoaderException("Cannot save the config if the 'nested' paramter is True!")
+
         if self.__loaded_config_file is None:
             raise ConfigLoaderException("Load not called yet!")
 
