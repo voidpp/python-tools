0fc7460c827b02d8914411cedddadc23ccb3cc73
Lajos Santa <santa.lajos@gmail.com>
2018-09-19 19:45:07 +0200

    release 1.11.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name = "voidpp-tools",
     description = "various python tools",
-    version = "1.10.0",
+    version = "1.11.0",
     author = 'Lajos Santa',
     author_email = 'santa.lajos@coldline.hu',
     url = 'https://github.com/voidpp/python-tools.git',
